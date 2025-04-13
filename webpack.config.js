1f1561d12a37b079d91146dd5bfae0adb6387121
Ivan Pivnenko <i.pivnenko@gmail.com>
2025-04-13 20:11:03 +0300

    update

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
         filename: "bundle.js",
         path: path.resolve(__dirname, "dist"),
         clean: true,
+        publicPath: isProd ? "/aprioriacquit.github.io/" : "/",
     },
     resolve: {
         extensions: [".ts", ".tsx", ".js", ".jsx"],
